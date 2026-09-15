707bbaece7d6dfaf1dc2c36c207b4a07962034f7
shahidalikn62-gif <shahidalikn62@gmail.com>
2026-09-15 15:12:49 +0530

    app.py

--- app.py
+++ app.py
@@ -56,12 +56,12 @@ for index, col_name in enumerate(X.columns):
   min_v= X[col_name].min()
   max_v= X[col_name].max()
   if col_name != "Previous_Insurance":
-  value = st.sidebar.slider(f"select value for {col_name}",
-                            min_value = min_v,
-                            max_value = max_v)
-else:
-  value = st.sidebar.number_input(f"select value for {col_name} (0:No, 1:Yes): ")
-  all_ans.append(value)
+    value = st.sidebar.slider(f"select value for {col_name}",
+                              min_value = min_v,
+                              max_value = max_v)
+  else:
+    value = st.sidebar.number_input(f"select value for {col_name} (0:No, 1:Yes): ")
+    all_ans.append(value)
 ud={j:all_ans[i] for i,j in enumerate(X.columns)}
 user_df  = pd.DataFrame(ud, index=[1])
 sr.write(user_df)
